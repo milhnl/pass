d0a009bd0c44cd8f338b0870b19bda597fee8d25
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2018-03-08 19:23:23 +0100

    Add list stub

--- pass.sh
+++ pass.sh
@@ -10,11 +10,26 @@ warn() { [ "$verbosity" -ge "0" ] && printf '%s\n' "$*" >&2; true;}
 err() { [ "$verbosity" -ge "-1" ] && printf '%s\n' "$*" >&2; true;}
 die() { [ "$verbosity" -ge "-2" ] && printf '%s\n' "$*" >&2; exit 1; }
 
+# POLYFILLS -------------------------------------------------------------------
+if ! command tree >/dev/null 2>&1; then
+    tree() {(
+            cd "$(dirname "$1")"
+            find "$(basename "$1")" -print 2>/dev/null | awk '
+                !/\.$/ { \
+                    for (i=1; i<NF; i++) { \
+                        printf("%4s", "|") \
+                    } \
+                    print "-- "$NF \
+                }
+            ' FS='/'
+    )}
+fi
+
 # COMMANDS --------------------------------------------------------------------
 pass_init() { die "Not implemented"; }
 
 pass_ls() { pass_list "$@"; }
-pass_list() { die "Not implemented"; }
+pass_list() { tree "$PASSWORD_STORE_DIR"; }
 
 pass_grep() { die "Not implemented"; }
 
