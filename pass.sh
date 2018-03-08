7da9da671405f171cf41f2ddf4cdde40cb5cf96a
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2018-03-08 01:34:40 +0100

    Add command synonyms from the other pass

--- pass.sh
+++ pass.sh
@@ -10,22 +10,41 @@ warn() { [ "$verbosity" -ge "0" ] && printf '%s\n' "$*" >&2; true;}
 err() { [ "$verbosity" -ge "-1" ] && printf '%s\n' "$*" >&2; true;}
 die() { [ "$verbosity" -ge "-2" ] && printf '%s\n' "$*" >&2; exit 1; }
 
+# COMMANDS --------------------------------------------------------------------
 pass_init() { die "Not implemented"; }
+
 pass_ls() { pass_list "$@"; }
 pass_list() { die "Not implemented"; }
+
 pass_grep() { die "Not implemented"; }
+
+pass_search() { pass_find "$@"; }
 pass_find() { die "Not implemented"; }
+
 pass_show() { die "Not implemented"; }
+
+pass_add() { pass_insert "$@"; }
 pass_insert() { die "Not implemented"; }
+
 pass_edit() { die "Not implemented"; }
+
 pass_generate() { die "Not implemented"; }
-pass_rm() { die "Not implemented"; }
-pass_mv() { die "Not implemented"; }
-pass_cp() { die "Not implemented"; }
+
+pass_rm() { pass_remove "$@"; }
+pass_delete() { pass_remove "$@"; }
+pass_remove() { die "Not implemented"; }
+
+pass_mv() { pass_rename "$@"; }
+pass_rename() { die "Not implemented"; }
+
+pass_cp() { pass_copy "$@"; }
+pass_copy() { die "Not implemented"; }
+
 pass_git() { die "Not implemented"; }
+
 pass_help() { die "Not implemented"; }
+
 pass_version() { die "Not implemented"; }
 
-PASS_CMD="$1"; shift
-"pass_$PASS_CMD" "$@"
+pass_"$@"
 
